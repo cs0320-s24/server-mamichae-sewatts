1bc12301653624bdaa08c76376c5f5de4a264ae0
sylviewatts <sylvie_watts@brown.edu>
2024-02-11 13:24:26 -0500

    feat: Classes set up.

---
+++ src/main/java/census/Census.java
@@ -0,0 +1,9 @@
+package census;
+
+/**
+ * This is a class that models an Activity received from the API.
+ */
+
+
+public class Census {
+}
---
+++ src/main/java/census/CensusAPIUtilities.java
@@ -0,0 +1,8 @@
+package census;
+
+/**
+ * deserializing JSON from the API into a Census return thing.
+ */
+
+public class CensusAPIUtilities {
+}

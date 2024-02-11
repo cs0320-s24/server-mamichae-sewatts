615d8fc418c80e415b33979edb10df8d2080aa8b
sylviewatts <sylvie_watts@brown.edu>
2024-02-11 13:45:14 -0500

    feat: CountyAccess updated

--- src/main/java/countyAccess/CountyAccessAPIUtilities.java
+++ src/main/java/countyAccess/CountyAccessAPIUtilities.java
@@ -1,8 +1,8 @@
-package census;
+package countyAccess;
 
 /**
  * deserializing JSON from the API into a Census return thing.
  */
 
-public class CensusAPIUtilities {
+public class CountyAccessAPIUtilities {
 }

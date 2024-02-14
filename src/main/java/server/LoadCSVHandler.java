df20c9f195050d060834daa96c56f0662e0c06d5
Melanie Michael <melanie_michael@brown.edu>
2024-02-14 11:59:04 -0500

    feat: added functionality to Load and Search handlers

--- src/main/java/server/LoadCSVHandler.java
+++ src/main/java/server/LoadCSVHandler.java
@@ -1,16 +1,57 @@
 package server;
 
+import CSV.CSVParser;
+import CSV.FactoryFailureException;
 import CSV.InformationOnCSV;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.ProcessHandle.Info;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class LoadCSVHandler implements Route {
 
-    private InformationOnCSV info;
+    private InformationOnCSV csv;
+
+    public LoadCSVHandler(InformationOnCSV csv) {
+        this.csv = csv;
+    }
+
     @Override
     public Object handle(Request request, Response response) {
-        return null;
+        String filepath = request.queryParams("filepath");
+        Boolean hasHeaders = Boolean.valueOf(request.queryParams("headers"));
+        List<String> headers = new ArrayList<>();
+        Map<String, String> responseMap = new HashMap<>();
+
+        // need buffered? or just file reader
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            // what to add for object?
+            // new creator from row?
+            CSVParser parser = new CSVParser(reader, hasHeaders);
+            List<List<String>> parsedData = parser.parse();
+
+            this.csv.setParsedText(parsedData);
+            if (hasHeaders) {
+                this.csv.setHeaders(parser.getHeaderList());
+            }
+            this.csv.setLoaded(true);
+
+            responseMap.put("result", "success");
+            responseMap.put("filepath", filepath);
+        } catch (IOException e) {
+            responseMap.put("error", "IO error occurred: " + e.getMessage());
+        } catch (FactoryFailureException e) {
+            responseMap.put("error", "Failed to create CSV parser: " + e.getMessage());
+        }
+
+        return responseMap;
     }
 
 }
--- src/main/java/server/SearchCSVHandler.java
+++ src/main/java/server/SearchCSVHandler.java
@@ -1,12 +1,84 @@
 package server;
 
+import CSV.CSVParser;
+import CSV.CSVSearcher;
+import CSV.FactoryFailureException;
+import CSV.InconsistentRowException;
+import CSV.InformationOnCSV;
+import CSV.NotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 
 public class SearchCSVHandler implements Route {
-    @Override
-    public Object handle(Request request, Response response) {
-        return null;
+    private final InformationOnCSV csv;
+
+    public SearchCSVHandler(InformationOnCSV csv) {
+        this.csv = csv;
     }
+
+    @Override
+    public Object handle(Request request, Response response) throws DatasourceException {
+        String searchValue = request.queryParams("value");
+        String columnIdentifier = request.queryParams("columnID");
+
+        List<List<String>> searchResult = null;
+        Map<String, Object> responseMap = new HashMap<>();
+
+        try {
+            if (this.csv.getLoaded()) {
+                // how to change to either input parser object
+                // or change parameters
+                // no casting
+                CSVSearcher searcher = new CSVSearcher(this.csv.getParsedText());
+                if (columnIdentifier != null && !columnIdentifier.isEmpty()) {
+                    try {
+                        int columnIndex = Integer.parseInt(columnIdentifier);
+                        searchResult = searcher.search(searchValue, columnIndex);
+                    } catch (NumberFormatException e) {
+                        searchResult = searcher.search(searchValue, columnIdentifier);
+                    }
+                } else {
+                    // Search the entire CSV
+                    searchResult = searcher.search(searchValue);
+                }
+
+                responseMap.put("result", "success");
+                responseMap.put("data", searchResult);
+            } else {
+                responseMap.put("error", "no CSV loaded");
+            }
+        } catch (IOException e) {
+            responseMap.put("error", "error while processing data: " + e.getMessage());
+        }
+
+        return toJson(responseMap);
+    }
+    private List<List<String>> performSearch(List<List<String>> parsedData, List<String> headers, String value, String columnIdentifier)
+        throws IOException, NotFoundException, InconsistentRowException, FactoryFailureException {
+        CSVSearcher searcher = new CSVSearcher(parsedData);
+
+        if (columnIdentifier.equalsIgnoreCase("none")) {
+            return searcher.search(value);
+        } else {
+            try {
+                return searcher.search(value, columnIdentifier);
+            } catch (NumberFormatException | NotFoundException e) {
+                return searcher.search(value, columnIdentifier);
+            }
+        }
+    }
+
+    private String toJson(Object object) {
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Object> adapter = moshi.adapter(Object.class);
+        return adapter.toJson(object);
+    }
+
 }

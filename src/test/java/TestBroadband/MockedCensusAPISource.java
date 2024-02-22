5fac640711f5bf8ec5238ef5eb8c6bf689a2f1de
sylviewatts <sylvie_watts@brown.edu>
2024-02-22 17:07:47 -0500

    trying to pull

--- src/test/java/TestBroadband/MockedCensusAPISource.java
+++ src/test/java/TestBroadband/MockedCensusAPISource.java
@@ -6,9 +6,7 @@ import countyAccess.LocationData;
 import server.DatasourceException;
 
 /**
- * A datasource that never actually calls the NWS API, but always returns a constant
- * weather-data value. This is very useful in testing, and avoiding the costs of
- * real API invocations. The technique is called "mocking", as in "faking".
+ *
  */
 public class MockedCensusAPISource implements CensusDataSource {
   private final AccessData constantData;
